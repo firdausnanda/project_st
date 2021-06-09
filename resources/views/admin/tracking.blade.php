aaeddac19beee86f65199b5c1072c7be1287f1d0
firdausnanda <firdausnanda46@gmail.com>
2021-06-09 08:48:50 +0700

    add-menu-rekapmatkul

--- resources/views/admin/tracking.blade.php
+++ resources/views/admin/tracking.blade.php
@@ -110,6 +110,39 @@
                 </a>
             </li>
 
+            <li class="menu">
+                <a href="#elements" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle collapsed" style="text-decoration: none;">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+                        <span>Report</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-chevron-right">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                    </div>
+                </a>
+                <ul class="submenu list-unstyled collapse" id="elements" data-parent="#accordionExample" style="">
+                    <li>
+                        <a href="#" style="text-decoration: none;">Rekap Matakuliah</a>
+                    </li>
+                    <li>
+                        <a href="#" style="text-decoration: none;">Rekap Dosen</a>
+                    </li>
+                    <li>
+                        <a href="#" style="text-decoration: none;">Rekap Pembimbingan</a>
+                    </li>
+                    <li>
+                        <a href="#" style="text-decoration: none;">Rekap Penunjang</a>
+                    </li>
+                    <li>
+                        <a href="#" style="text-decoration: none;">Rekap Dosen Total</a>
+                    </li>
+                </ul>
+            </li>
+
         </ul>
         <!-- <div class="shadow-bottom"></div> -->
 
